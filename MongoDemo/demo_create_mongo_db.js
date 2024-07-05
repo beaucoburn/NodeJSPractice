36f230c678813b2b5b20db54c487beab11a3a717
beaucoburn <douglas.coburn@gmail.com>
2024-07-05 14:04:27 -0600

    new file:   demo_create_mongo_db.js

---
+++ MongoDemo/demo_create_mongo_db.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+
+var mongoURI = "mongodb://localhost:27017/test";
+var MongoDB = mongoose.connect(mongoURI).connection;
+MongoDB.on('error', function(err) { console.log(err.message); });
+MongoDB.once('open', function() {
+  console.log("mongodb connection open");
+});
+
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
